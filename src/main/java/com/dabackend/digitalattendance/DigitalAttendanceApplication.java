90dac07a39b9589be3787847f5198258194b5d45
Donmbuya9 <128780541+Donmbuya9@users.noreply.github.com>
2025-08-25 15:54:39 +0300

    initial project set up

---
+++ src/main/java/com/dabackend/digitalattendance/DigitalAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.dabackend.digitalattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalAttendanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalAttendanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dabackend/digitalattendance/DigitalAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dabackend.digitalattendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalAttendanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
